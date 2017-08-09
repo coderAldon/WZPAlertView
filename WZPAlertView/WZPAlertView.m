0f3313a171ebdcb43c626035baa9ef30c9952389
coderAldon <18865732781@163.com>
2017-08-09 11:51:11 +0800

    no message

--- WZPAlertView/WZPAlertView.m
+++ WZPAlertView/WZPAlertView.m
@@ -258,7 +258,7 @@
     animation.removedOnCompletion = YES;
     animation.fillMode = kCAFillModeForwards;
     NSMutableArray *values = [NSMutableArray array];
-    [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(0.9, 0.9, 1.0)]];
+    [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(0.8, 0.8, 1.0)]];
     [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(1.1, 1.1, 1.0)]];
     [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 1.0)]];
     animation.values = values;
